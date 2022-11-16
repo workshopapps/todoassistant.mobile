b7a5a32884424dc8ff5cc1eae2961f32fc80c6de
Mogakamo <mogaka.amo254@gmail.com>
2022-11-16 18:41:35 +0300

    [setup]: project initialisation

---
+++ ticked/src/screens/HomeScreen/index.jsx
@@ -0,0 +1,33 @@
+import { View, Text, Button } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import styles from "./index.styles";
+import { useDispatch, useSelector } from "react-redux";
+
+const HomeScreen = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  const navigate = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+      <Button title="Profile" onPress={() => navigate.navigate("Profile")} />
+
+      <Text>Count: {count}</Text>
+      <View style={styles.button}>
+        <Button
+          title="Increment"
+          onPress={() => dispatch({ type: "counter/increment" })}
+        />
+        <Button
+          title="Decrement"
+          onPress={() => dispatch({ type: "counter/decrement" })}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default HomeScreen;
