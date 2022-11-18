8a915924a0fb40c88062c0dfdb3a0bdd00836804
mystry22 <alanemehenry6@gmail.com>
2022-11-18 07:25:47 -0800

    profiles screens

--- ticked/src/navigation/MainStackNavigator.jsx
+++ ticked/src/navigation/MainStackNavigator.jsx
@@ -1,7 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { HomeScreen, ProfileScreen } from '../screens'
+import { HomeScreen, ProfileScreen } from '../screens';
+import MyProfile from '../screens/Profile/ProfileLayout';
+import ChangePassword from '../screens/Profile/ChangePassword';
+import EditProfile from '../screens/Profile/EditProfile';
 
 
 const Stack = createNativeStackNavigator()
@@ -10,6 +13,9 @@ const MainStackNavigator = () => {
     <Stack.Navigator>
       <Stack.Screen name='Home' component={HomeScreen} />
       <Stack.Screen name='Profile' component={ProfileScreen} />
+      <Stack.Screen name='My profile' component={MyProfile} />
+      <Stack.Screen name='Change password' component={ChangePassword} />
+      <Stack.Screen name='Edit Profile' component={EditProfile} />
     </Stack.Navigator>
   )
 }
---
+++ ticked/src/screens/Profile/ProfileComponents/EditProfileComponent.js
@@ -0,0 +1,77 @@
+import { View, Text, StyleSheet,  } from 'react-native';
+import React from 'react';
+import { colors } from '../../../utils/colors'
+import { Input } from '../../../components/Input/index';
+import {Button} from '../../../components/Button';
+
+const ProfileLayoutComponent = () => {
+    return (
+        <View style={style.container}>
+
+            {/* Upper section */}
+            <View style={style.upperProfile}>
+                <View style={style.profileCap}>
+                    <Text style={{ color: '#707070', fontSize: 50 }}>J</Text>
+                </View>
+
+
+                 {/* form section */}
+
+            <Input
+                label='Full name'
+                placeholder={'Full Name'}
+                value='Joseph Anderson'
+
+            />
+
+            <Input
+                label='Email Address'
+                placeholder={'example@gmail.com'}
+                value='Josephanderson@gmail.com'
+
+            />
+
+            <Input
+                label='Phone Number'
+                placeholder={'081 234 5678'}
+                value='081 234 5678'
+
+            />
+            </View>
+
+           
+
+
+
+
+        <Button title={'Save Changes'} />
+
+
+        </View>
+    )
+}
+
+export default ProfileLayoutComponent
+
+const style = StyleSheet.create({
+    container: {
+        height: '100%',
+        backgroundColor: colors.backgroundColor,
+        padding: 20
+    },
+    upperProfile: {
+        marginTop: 70,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10
+    },
+    profileCap: {
+        width: 80,
+        height: 80,
+        borderRadius: 100,
+        backgroundColor: colors.profileCircle,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+});
