8a507647095ecf37fdc688a9dd9afc77c94ef35b
Mogakamo <mogaka.amo254@gmail.com>
2022-11-18 10:33:07 +0300

    [package]: added ts support

--- ticked/src/layouts/AuthLayout.tsx
+++ ticked/src/layouts/AuthLayout.tsx
@@ -1,13 +1,9 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { ReactNode } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const AuthLayout = ({children}: ReactNode) => {
-  return (
-    <SafeAreaView>
-        {children}
-    </SafeAreaView>
-  );
+const AuthLayout = ({ children }: { children: ReactNode }) => {
+  return <SafeAreaView>{children}</SafeAreaView>;
 };
 
 export default AuthLayout;
