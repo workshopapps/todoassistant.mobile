a220a97b1a7e72ceb12574b040ba7178c06cbaa7
Mogakamo <mogaka.amo254@gmail.com>
2022-11-18 11:41:16 +0300

    [UI]: Registration Screen

--- ticked/src/navigation/MainStackNavigator.jsx
+++ ticked/src/navigation/MainStackNavigator.jsx
@@ -1,17 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { HomeScreen, ProfileScreen } from '../screens'
+import { View, Text } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen, ProfileScreen, RegistrationScreen } from "../screens";
 
-
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 const MainStackNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Home' component={HomeScreen} />
-      <Stack.Screen name='Profile' component={ProfileScreen} />
+      <Stack.Screen name="Registration" component={RegistrationScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default MainStackNavigator
+export default MainStackNavigator;
