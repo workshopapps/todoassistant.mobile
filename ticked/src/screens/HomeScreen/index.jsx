124907f0c421d0dfa5899931335beddcc66a0ea7
Mogakamo <mogaka.amo254@gmail.com>
2022-11-19 05:49:50 +0300

    chore: arranging the layout

--- ticked/src/screens/HomeScreen/index.jsx
+++ ticked/src/screens/HomeScreen/index.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import styles from "./index.styles";
 import { useDispatch, useSelector } from "react-redux";
+import { MainLayout } from "../../layouts";
 
 const HomeScreen = () => {
   const count = useSelector((state) => state.counter.value);
@@ -11,22 +12,24 @@ const HomeScreen = () => {
   const navigate = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
-      <Button title="Profile" onPress={() => navigate.navigate("Profile")} />
+    <MainLayout>
+      <View style={styles.container}>
+        <Text>HomeScreen</Text>
+        <Button title="Profile" onPress={() => navigate.navigate("Profile")} />
 
-      <Text>Count: {count}</Text>
-      <View style={styles.button}>
-        <Button
-          title="Increment"
-          onPress={() => dispatch({ type: "counter/increment" })}
-        />
-        <Button
-          title="Decrement"
-          onPress={() => dispatch({ type: "counter/decrement" })}
-        />
+        <Text>Count: {count}</Text>
+        <View style={styles.button}>
+          <Button
+            title="Increment"
+            onPress={() => dispatch({ type: "counter/increment" })}
+          />
+          <Button
+            title="Decrement"
+            onPress={() => dispatch({ type: "counter/decrement" })}
+          />
+        </View>
       </View>
-    </View>
+    </MainLayout>
   );
 };
 
--- ticked/src/screens/index.js
+++ ticked/src/screens/index.js
@@ -1,2 +1,3 @@
 export { default as HomeScreen } from "./HomeScreen";
 export { default as ProfileScreen } from "./ProfileScreen";
+export { default as SettingScreen } from "./SettingsScreen";
