75321c5229891fea208c07803b72109096ff4782
Mogakamo <mogaka.amo254@gmail.com>
2022-11-19 06:17:14 +0300

    [UI]: working on the HomeScreen

--- ticked/src/screens/HomeScreen/index.jsx
+++ ticked/src/screens/HomeScreen/index.jsx
@@ -5,6 +5,7 @@ import styles from "./index.styles";
 import { useDispatch, useSelector } from "react-redux";
 import { MainLayout } from "../../layouts";
 import { Input } from "../../components/Input";
+import Icon from "react-native-vector-icons/EvilIcons";
 
 const HomeScreen = () => {
   const count = useSelector((state) => state.counter.value);
@@ -14,8 +15,22 @@ const HomeScreen = () => {
 
   return (
     <MainLayout>
-      <View>
-        <Input />
+      <View style={styles.container}>
+        <View style={styles.search}>
+          <Icon name="search">
+            <Input style={styles.search} />
+          </Icon>
+        </View>
+        {/* Stats */}
+        <View>
+          <Text>Your Progress</Text>
+        </View>
+        <View>
+          <Text>Tasks</Text>
+        </View>
+        <View>
+          <Text>Completed 2</Text>
+        </View>
       </View>
     </MainLayout>
   );
--- ticked/src/screens/HomeScreen/index.styles.js
+++ ticked/src/screens/HomeScreen/index.styles.js
@@ -2,11 +2,8 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   container: {
-    padding: 10,
+    paddingHorizontal: 10,
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
   button: {
     flexDirection: "row",
