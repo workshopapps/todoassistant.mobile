0a97c5e598166d961c3ec3d949718baded809f2e
Mogakamo <mogaka.amo254@gmail.com>
2022-11-19 21:36:26 +0300

    [UI]: added a new navigation flow

--- ticked/src/screens/HomeScreen/index.jsx
+++ ticked/src/screens/HomeScreen/index.jsx
@@ -1,6 +1,6 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 import styles from "./index.styles";
 import { useDispatch, useSelector } from "react-redux";
 import home1 from "../../assets/home1.png";
@@ -11,7 +11,6 @@ import Icon from "react-native-vector-icons/EvilIcons";
 import { Tasks } from "../../components";
 import FireIcon from "react-native-vector-icons/SimpleLineIcons";
 
-
 const HomeScreen = () => {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
@@ -19,57 +18,6 @@ const HomeScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <View
-      style={{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "˛center",
-        paddingTop: 60,
-        paddingLeft:20,
-        paddingRight:20,
-        backgroundColor: "#ffffff",
-      }}
-    >
-      <View style={{display: "flex", flexDirection:"row", justifyContent: "flex-end", paddingRight: 20, }} >
-      <Text style={{color:"#714DD9", fontSize:14}}> Skip</Text></View>
-      <Image
-        source={home1}
-        style={{
-          flex: 3,
-          width: "100%",
-        }}
-      />
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          paddingTop: 20,
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 18,
-            fontWeight: "bold",
-            alignItems: "center",
-            justifyContent: "center",
-            paddingBottom: 8,
-          }}
-        >
-          Managing your task the easy way
-        </Text>
-        <Text style={{ fontSize: 14, paddingBottom: 5 }}>
-          {" "}
-          The most useful to-do-list app that breaks tasks
-        </Text>
-        <Text style={{ fontSize: 14, paddingBottom: 30 }}>
-          {" "}
-          into small steps.
-        </Text>
-        <Button onPress={() => navigation.navigate('Home2')} style={{ fontSize: 14 }} title="Next" />
     <MainLayout>
       <View style={styles.container}>
         <View style={styles.search}>
@@ -83,23 +31,23 @@ const HomeScreen = () => {
           <View style={styles.cardFlex}>
             <View style={styles.card}>
               <View style={styles.flexRow}>
-              <View>
-                <Text style={styles.bold}>3/7 to do</Text>
-                <Text style={styles.grey}>ticked</Text>
-              </View>
-              <View style={styles.chart}>
-                <Icon name="chart" size={20} backgroundColor="#fff" />
-              </View>
+                <View>
+                  <Text style={styles.bold}>3/7 to do</Text>
+                  <Text style={styles.grey}>ticked</Text>
+                </View>
+                <View style={styles.chart}>
+                  <Icon name="chart" size={20} backgroundColor="#fff" />
+                </View>
               </View>
             </View>
             <View style={styles.card}>
               <View style={styles.flexRow}>
-              <View>
-                <Text style={styles.bold}>7 Day streak</Text>
-              </View>
-              <View style={styles.fire}>
-                <FireIcon name="fire" backgroundColor="#FDA758" size={20} />
-              </View>
+                <View>
+                  <Text style={styles.bold}>7 Day streak</Text>
+                </View>
+                <View style={styles.fire}>
+                  <FireIcon name="fire" backgroundColor="#FDA758" size={20} />
+                </View>
               </View>
             </View>
           </View>
@@ -108,7 +56,6 @@ const HomeScreen = () => {
           <Text style={styles.bold}>Tasks</Text>
           <Tasks task="Resolve frontend bugs" time="4 hrs" />
         </View>
-       
       </View>
     </MainLayout>
   );
--- ticked/src/screens/index.js
+++ ticked/src/screens/index.js
@@ -1,9 +1,8 @@
 export { default as HomeScreen } from "./HomeScreen";
 export { default as ProfileScreen } from "./ProfileScreen";
-export { default as Screen2 } from "./Screen2";
+export { default as Screen2 } from "./Onboarding/Screen2";
 export { default as Auth } from "./Auth";
 export { default as Login } from "./Login";
-
-
 export { default as SettingScreen } from "./SettingsScreen";
 export { default as OtpScreen } from "./OtpScreen";
+export { default as OnboardingScreen1} from "./Onboarding/OnboardingScreen1";
