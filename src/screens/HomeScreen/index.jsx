d4fb6d7620211251c2cc0b7a1a3da4eac48b194e
mogakamo <mogaka.amo254@gmail.com>
2022-12-01 16:31:21 +0300

    [ui]: updated the tabs navigator

--- src/screens/HomeScreen/index.jsx
+++ src/screens/HomeScreen/index.jsx
@@ -2,15 +2,34 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { MainLayout } from '../../layouts'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import AllTasksScreen from './Tasks/AllTasksScreen';
+import AssistantReminders from './Tasks/AssistantReminders';
 
 const Tab = createMaterialTopTabNavigator();
 
 const HomeScreen = () => {
-  return (
-    <MainLayout>
-      <Text>HomeScreen</Text>
-    </MainLayout>
-  )
+    return (
+        <MainLayout>
+            <Tab.Navigator
+                screenOptions={{
+                    tabBarLabelStyle: {
+                        fontSize: 14,
+                        color: '#000',
+                        focused: true,
+                        textTransform: 'capitalize',
+                    },
+                    tabBarIndicatorStyle: {
+                        backgroundColor: '#714DD9',
+                    },
+                    tabBarActiveTintColor: '#FDA758',
+                    tabBarStyle: { backgroundColor: 'none', width: '65%' },
+                }}>
+                <Tab.Screen name="All" component={AllTasksScreen} />
+                <Tab.Screen name="Mine" component={AllTasksScreen} />
+                <Tab.Screen name="Assistant" component={AssistantReminders} />
+            </Tab.Navigator>
+        </MainLayout>
+    )
 }
 
 export default HomeScreen
