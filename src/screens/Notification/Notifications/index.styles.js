8afdb3c621c65478c06ca02dbc411c19ee94e6e1
Mogakamo <mogaka.amo254@gmail.com>
2022-12-04 07:26:00 +0300

    ui: notifications screen is up

--- src/screens/Notification/Notifications/index.styles.js
+++ src/screens/Notification/Notifications/index.styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   container: {
     paddingVertical: 20,
-    backgroundColor: "#F9F7FF",
+    backgroundColor: '#F9F7FF',
     flex: 1,
   },
 });
