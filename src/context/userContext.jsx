bd6fa7c693a3bcdedf353d4291c042b0ff289284
Ihuoma Favour Agbaru <51078475+fynefav3@users.noreply.github.com>
2022-12-05 11:27:22 +0100

    Feat  field validation (#30)
    * Profile screen started MOB-15
    
    * feat: Edit page added MOB-16
    
    * Fixed the edit screen
    
    * Fixed
    
    * Feat: User can update their details
    
    * feat: validation has been added
    
    * node install done

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,9 +1,9 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import React, { createContext, useState, useEffect } from 'react';
-// eslint-disable-next-line import/namespace
 import { Alert } from 'react-native';
 
+import { validatePwd, validateEmail, formValidation } from '../screens/Validation';
 import { BASE_URL } from '../utils/config';
 
 export const AuthContext = createContext();
@@ -14,58 +14,82 @@ export const AuthProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState(null);
 
   const login = (email, password) => {
-    setIsLoading(true);
-    axios
-      .post(`${BASE_URL}/user/login`, {
-        email,
-        password,
-      })
-      .then((res) => {
-        console.log(res.data);
-        let userInfo = res.data;
-        setUserInfo(userInfo);
-        // setUserToken(userInfo.access_token);
+    if (validateEmail(email) && formValidation(password)) {
+      setIsLoading(true);
 
-        AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
-        AsyncStorage.setItem('userToken', userInfo.access_token);
-      })
-      .catch((err) => {
-        Alert(err.response.data.error.error)
-      });
+      console.log('logging in');
 
-    setUserToken('fakdjfha');
-    //
-    setIsLoading(false);
+      axios
+        .post(`${BASE_URL}/user/login`, {
+          email,
+          password,
+        })
+        .then((res) => {
+          console.log(res.data);
+          const userInfo = res.data;
+          setUserInfo(userInfo);
+
+          // setUserToken(userInfo.access_token);
+
+          AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
+          AsyncStorage.setItem('userToken', userInfo.access_token);
+          // setIsLoading(false);
+        })
+        .catch((err) => {
+          Alert(err.response.data.error.error);
+          // setIsLoading(false);
+        });
+
+      setUserToken('fakdjfha');
+      //
+      setIsLoading(false);
+    }
   };
 
   const register = (first_name, last_name, email, password, date_of_birth, phone, gender) => {
-    setIsLoading(true);
-    axios
-      .post(`${BASE_URL}/user`, {
-        first_name,
-        last_name,
-        email,
-        password,
-        date_of_birth,
-        phone,
-        gender,
-      })
-      .then((res) => {
-        console.log(res.data);
-        Alert.alert('Success', res.data.message);
+    if (
+      first_name !== null &&
+      last_name !== null &&
+      email !== null &&
+      phone !== null &&
+      gender !== null &&
+      date_of_birth !== null &&
+      password !== null
+    ) {
+      if (validateEmail(email) && validatePwd(password)) {
+        setIsLoading(true);
+        axios
+          .post(`${BASE_URL}/user`, {
+            first_name,
+            last_name,
+            email,
+            password,
+            date_of_birth,
+            phone,
+            gender,
+          })
+          .then((res) => {
+            console.log(res.data);
+            Alert.alert('Success', res.data.message);
 
-        let userInfo = res.data;
-        setUserInfo(userInfo);
-        setUserToken(userInfo.access_token);
+            const userInfo = res.data;
 
-        AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
-        AsyncStorage.setItem('userToken', userInfo.access_token);
-      })
-      .catch((err) => {
-        Alert(err);
-      });
+            setUserInfo(userInfo);
+            setUserToken(userInfo.access_token);
 
-    setIsLoading(false);
+            AsyncStorage.setItem('userInfo', JSON.stringify(userInfo));
+            AsyncStorage.setItem('userToken', userInfo.access_token);
+
+            setIsLoading(false);
+          })
+          .catch((err) => {
+            Alert(err);
+            setIsLoading(false);
+          });
+      }
+    } else {
+      alert('Some field are empty');
+    }
   };
 
   const logout = () => {
@@ -80,7 +104,7 @@ export const AuthProvider = ({ children }) => {
     try {
       setIsLoading(true);
       let userInfo = await AsyncStorage.getItem('userInfo');
-      let userToken = await AsyncStorage.getItem('userToken');
+      const userToken = await AsyncStorage.getItem('userToken');
 
       userInfo = JSON.parse(userInfo);
 
@@ -96,12 +120,56 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
+  const newTask = async (title, description, end_time, va_option) => {
+    if (
+      formValidation(title) &&
+      formValidation(description) &&
+      formValidation(end_time) &&
+      formValidation(va_option)
+    ) {
+      // setIsLoading(true);
+
+      const payload = {
+        title,
+        description,
+        start_time: new Date(),
+        repeat: 'daily',
+        end_time,
+        va_option,
+      };
+
+      console.log(payload);
+
+      axios
+        .post(`${BASE_URL}/task`, payload, {
+          headers: {
+            Authorization: `Bearer ${await AsyncStorage.getItem('userToken')}`,
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          // setIsLoading(false);
+
+          return true;
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+          // setIsLoading(false);
+
+          return false;
+        });
+
+      return true;
+    }
+  };
+
   useEffect(() => {
     isLoggedIn();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ login, logout, isLoading, userToken, userInfo, register }}>
+    <AuthContext.Provider
+      value={{ login, logout, isLoading, userToken, userInfo, register, newTask }}>
       {children}
     </AuthContext.Provider>
   );
