49896304720d197c5de5e5c775f3c8e6ab2e9c2a
Amos <mogaka.amo254@gmail.com>
2023-02-06 17:00:57 +0300

    [UI]: Reset Password Flow (#42)
    * '
    
    * [commit]: latest changes
    
    * commit
    
    * UI: Reset Password Flow(#38) (#40)
    
    * Latest changes (#38)
    
    * '
    
    * [commit]: latest changes
    
    * [commitPassword]
    
    * [lint]: updated the order of imports 
    
    Changed the order of imports to avoid lint issues. The order should be ordered alphabetically and in a descending order.
    
    * commit
    
    * Rename ForgotpasswordSCreen.tsx to ForgotPasswordScreen.tsx
    
    * [commitPassword]
    
    ---------
    
    Co-authored-by: Amos <mogaka.amo254@gmail.com>
    
    ---------
    
    Co-authored-by: Faith-Watiri <98256444+Faith-Watiri@users.noreply.github.com>

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,7 +1,9 @@
+export {default as ForgotPasswordScreen} from './Password/ForgotPasswordScreen';
 export {default as HomeScreen} from './HomeScreen';
-export {default as LoginScreen} from './Auth/LoginScreen'
+export {default as LoginScreen} from './Auth/LoginScreen';
 export {default as NotificationsScreen} from './NotificationsScreen';
 export {default as OnboardingScreen} from './OnboardingScreen';
 export {default as ProfileScreen} from './ProfileScreen';
-export {default as RegisterScreen} from "./Auth/RegisterScreen";
-export {default as SearchScreen} from './SearchScreen';
+export {default as RegisterScreen} from './Auth/RegisterScreen';
+export {default as ResetPasswordScreen} from './Password/ResetPasswordScreen';
+export {default as SearchScreen} from './SearchScreen'
