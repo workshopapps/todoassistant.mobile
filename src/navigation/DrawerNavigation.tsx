e6e0547345605dce98a8ca9fd61c8ce0f097529a
mogakamo <mogaka.amo254@gmail.com>
2023-02-09 11:08:53 +0300

    [fix]: reverting the file to initial navigation

--- src/navigation/DrawerNavigation.tsx
+++ src/navigation/DrawerNavigation.tsx
@@ -6,9 +6,11 @@ const Drawer = createDrawerNavigator()
 
 export const DrawerNavigation = () => {
     return (
-        <Drawer.Navigator screenOptions={{
-            headerShown: false
-        }}>
+        <Drawer.Navigator 
+        // screenOptions={{
+        //     headerShown: false
+        // }}
+        >
             <Drawer.Screen name="Home" component={HomeScreen} />
             <Drawer.Screen name="Search" component={SearchScreen} />
         </Drawer.Navigator>
