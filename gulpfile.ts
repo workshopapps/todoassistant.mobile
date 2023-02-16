8c300ed877f56851655f4f114a2c8df3fb299a28
Mogakamo <mogaka.amo254@gmail.com>
2023-02-16 04:52:21 +0300

    [fix]: app issues

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,10 +1,32 @@
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
+import gulp from 'gulp';
+// import del from 'del';
+import ts from 'gulp-typescript';
+import eslint from 'gulp-eslint';
 
-gulp.task('lint', function () {
+// Delete the dist directory
+// export function clean() {
+//   return del(['dist']);
+// }
+
+// Compile TypeScript files and move to dist directory
+export function compile() {
+  const tsProject = ts.createProject('tsconfig.json');
+  return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'));
+}
+
+// Run ESLint on the TypeScript files
+export function lint() {
   return gulp
-    .src(['**/*.js', '!node_modules/**'])
+    .src(['src/**/*.ts'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-});
+}
+
+// Watch for changes in TypeScript files and compile
+export function watch() {
+  gulp.watch('src/**/*.ts', gulp.series(lint, compile));
+}
+
+// Default task: clean, lint, compile, and watch
+export default gulp.series(lint, compile, watch);
---
+++ src/screens/Notifications/AllNotificationsScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const AllNotificationsScreen = () => {
+  return (
+    <View>
+      <Text>AllNotificationsScreen</Text>
+    </View>
+  );
+};
+
+export default AllNotificationsScreen;
